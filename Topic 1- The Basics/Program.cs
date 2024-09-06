1c3d2d0ba89abb50ead5b2bd1f73bd750bfb8a94
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-09-06 09:37:48 -0400

    done

--- Topic 1- The Basics/Program.cs
+++ Topic 1- The Basics/Program.cs
@@ -1,19 +1,28 @@
-﻿namespace Topic_1__The_Basics
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Topic_1__The_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Tiago
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("never gonna give you up");
-            Console.WriteLine("burger");
-            Console.WriteLine("enter to continue");
+            Console.WriteLine("Hello, It's me Tiago");
+            Console.WriteLine();
+            Console.WriteLine("Comments are generaly used" +
+                " for notes, reminders or suggestions ignord by " +
+                "the computer so it dosent try to run it.");
+
+            Console.WriteLine("");
+            Console.WriteLine("TTTTT v   v");
+            Console.WriteLine("  T   v   v");
+            Console.WriteLine("  T   v   v");
+            Console.WriteLine("  T   v   v");
+            Console.WriteLine("  T   v   v");
+            Console.WriteLine("  T    v v ");
+            Console.WriteLine("  T     v  ");
             Console.ReadLine();
 
-            Console.WriteLine();
-            Console.Write("Hello, World");
-            Console.Write("What is Different");
         }
     }
 }
